16e2d26a89b708eed386e26c527c6b7dbe471be1
Simran Preet Singh <simransinghparmar@gmail.com>
2015-08-30 11:02:28 -0400

    jfdjgc

--- DWB.js
+++ DWB.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	$('.ws > p').hide();
+
+	$('.ws > h2').click(function(){
+		$('.glyph', this).toggleClass('glyphicon-chevron-up glyphicon-chevron-down');
+	});
 	$('.ws > h2').click(function(){
 		$(this).siblings().slideToggle();
 	});
 });
+
